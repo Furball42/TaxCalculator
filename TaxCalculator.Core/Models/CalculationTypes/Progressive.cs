4f362cee59d43805f7e5fbaeb0486100f99ff8e5
Marius Meyer <mariusmeyer42@gmail.com>
2021-06-12 09:56:26 +0200

    Most calculations done

--- TaxCalculator.Core/Models/CalculationTypes/Progressive.cs
+++ TaxCalculator.Core/Models/CalculationTypes/Progressive.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
@@ -12,7 +13,38 @@ namespace TaxCalculator.Core.Models.CalculationTypes
 
         public override  decimal CalculateResult(decimal annualIncome)
         {
-            throw new NotImplementedException();
+            if (annualIncome > 0)
+            {
+                var taxLevels = DeserializeExtendedData();
+
+                if (taxLevels.Count == 0)
+                    throw new Exception("Reference data empty");
+
+                var totalTax = 0m;
+                
+                foreach (var level in taxLevels)
+                {
+                    if (annualIncome > level.Min)
+                    {
+                        var taxableThisRate = Math.Min(level.Max - level.Min, annualIncome - level.Min);
+                        var taxOnThisLevel = taxableThisRate / 100 * level.Rate;
+                        totalTax += taxOnThisLevel;
+                    }
+                }
+
+                return totalTax;
+            }
+            else
+                throw new ArgumentException("No negative income value allowed.");
+        }
+
+        private List<ProgressiveTypeValues> DeserializeExtendedData()
+        {
+            if (string.IsNullOrEmpty(ExtendedData))
+                return new List<ProgressiveTypeValues>();
+
+            var xxx =  JsonConvert.DeserializeObject<List<ProgressiveTypeValues>>(ExtendedData);
+            return xxx;
         }
     }
 }
--- TaxCalculator.Test/CalculationTest.cs
+++ TaxCalculator.Test/CalculationTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -80,6 +81,62 @@ namespace TaxCalculator.Test
 
             Assert.IsNotNull(result);
             Assert.AreEqual(7500, result);
+        }
+
+        #endregion
+
+        #region Progressive
+
+        [Test]
+        public void Progressive_InputValue_SmallerThanZero_ArgumentException_Success()
+        {
+            var progressive = new Progressive
+            {
+                ExtendedData = BuildProgressiveData(),
+            };
+
+            Assert.Throws<ArgumentException>(() => progressive.CalculateResult(-200));
+        }
+
+        [Test]
+        public void Progressive_ReferenceDataCount_SmallerThanZero_Exception_Success()
+        {
+            var progressive = new Progressive
+            {
+                ExtendedData = string.Empty,
+            };
+
+            Assert.Throws<Exception>(() => progressive.CalculateResult(150000));
+        }
+
+        [Test]
+        public void Progressive_ShouldReturn_DynamicValue()
+        {
+            var progressive = new Progressive
+            {
+                ExtendedData = BuildProgressiveData(),
+            };
+
+            var result = progressive.CalculateResult(150000);
+
+            //TODO: maybe change all test to look for a list
+            Assert.IsNotNull(result);
+            Assert.AreEqual(35719.320, result);
+        }
+
+        private string BuildProgressiveData()
+        {
+            var unserializedList = new List<ProgressiveTypeValues>()
+            {
+                new ProgressiveTypeValues() { SortOrder = 1, Rate = 10, Min = 0, Max = 8350 },
+                new ProgressiveTypeValues() { SortOrder = 2, Rate = 15, Min = 8351, Max = 33950 },
+                new ProgressiveTypeValues() { SortOrder = 3, Rate = 25, Min = 33951, Max = 82250 },
+                new ProgressiveTypeValues() { SortOrder = 4, Rate = 28, Min = 82251, Max = 171550 },
+                new ProgressiveTypeValues() { SortOrder = 5, Rate = 33, Min = 171551, Max = 372950 },
+                new ProgressiveTypeValues() { SortOrder = 6, Rate = 35, Min = 372951, Max = -1 }
+            };
+
+            return JsonConvert.SerializeObject(unserializedList);
         }
 
         #endregion
