80a7b548de92a871270d85745c99dcdd54ae1e98
Marius Meyer <mariusmeyer42@gmail.com>
2021-06-13 11:53:08 +0200

    Added table to mvc to display saved records

--- TaxCalculator.API/Controllers/CalculationController.cs
+++ TaxCalculator.API/Controllers/CalculationController.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using TaxCalculator.Core.Dtos;
 using TaxCalculator.Core.Models;
+using TaxCalculator.Core.Models.CalculationResults;
 using TaxCalculator.Core.Services;
 
 namespace TaxCalculator.API.Controllers
@@ -25,6 +29,30 @@ namespace TaxCalculator.API.Controllers
         public async Task<CalculationResultDto> DoTaxCalculation(decimal annualIncome, string code)
         {
             return await _calculationService.ReturnDtoAndSave(annualIncome, code);
+        }
+
+        //generic call
+        [Route("GetCalculations")]
+        [HttpGet]
+        public async Task<List<CalculationResult>> GetCalculations()
+        {
+            var list = await _unitOfWork.CalculationResults.GetAll();
+            return list.ToList();
+        }
+
+        //specific for datatables
+        [Route("GetCalculationsForDatables")]
+        [HttpGet]
+        public async Task<DataTablesResponseDto> GetCalculationsForDatables()
+        {
+            var result = await _unitOfWork.CalculationResults.GetAll();
+            var list = result.ToList();
+
+            return new DataTablesResponseDto()
+            {
+                Data = list.ToArray(),
+                RecordsTotal = list.Count,
+            };
         }
     }
 }
